b27919a5c9911cfcda15b67003d110002ddf5711
Hilll19 <hilworking0110@gmail.com>
2023-08-19 00:25:58 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.getElementById("navbar");
+  const imgdropdown = document.getElementById("imgdropdown");
+  let prevScrollpos = window.pageYOffset;
+
+  navbar.style.top = "-150px";
+
+  window.onscroll = function () {
+    const currentScrollpos = window.pageYOffset;
+
+    if (currentScrollpos <= 25) {
+      navbar.style.top = "-150px";
+      imgdropdown.style.display = "block";
+    } else if (prevScrollpos > currentScrollpos) {
+      navbar.style.top = "0";
+      imgdropdown.style.display = "none";
+    } else {
+      navbar.style.top = "0";
+      imgdropdown.style.display = "none";
+    }
+
+    prevScrollpos = currentScrollpos;
+  };
+  const imgDropdownButton = document.querySelector(".imgdropdown-item");
+  const dropdownContent = document.getElementById("dropdownContent");
+
+  imgDropdownButton.addEventListener("click", function () {
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+
+  const carousel = document.getElementById("carouselExampleCaptions");
+  let activeSlideIndex = 0;
+
+  // Function to change the active slide
+  function changeSlide(index) {
+    const indicators = document.querySelectorAll(".carousel-indicators button");
+    indicators[activeSlideIndex].classList.remove("active");
+    carousel.querySelector(".carousel-item.active").classList.remove("active");
+
+    activeSlideIndex = index;
+
+    if (activeSlideIndex === slideImages.length - 3) {
+      // Reset to the first slide after reaching the last slide
+      activeSlideIndex = 0;
+    }
+
+    indicators[activeSlideIndex].classList.add("active");
+    carousel
+      .querySelectorAll(".carousel-item")
+      [activeSlideIndex].classList.add("active");
+  }
+
+  const slideImages = document.querySelectorAll(".carousel-item img");
+  slideImages.forEach((image, index) => {
+    image.addEventListener("click", () => {
+      changeSlide(index === slideImages.length - 3 ? 0 : index + 1);
+    });
+  });
+
+  // // Automatic slide transition every 6 seconds
+  // setInterval(() => {
+  //   changeSlide(activeSlideIndex + 1); // No need to handle wrapping here
+  // }, 6000); // Change the interval to 6000 milliseconds (6 seconds)
+});
