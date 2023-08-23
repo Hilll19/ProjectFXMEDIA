822547dcb63fc9a55d1e2ff4ead0b98b56d6c18d
Hilll19 <hilworking0110@gmail.com>
2023-08-23 13:16:07 +0700

    fix code, give some comment, tidy up the code

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Main Navbar logic, when scrolling show navbar and when scroll top 0  navbar hidden
+  // Navbar dropsown logic, when scrolling hidden and when scroll top 0 show
   const navbar = document.getElementById("navbar");
   const imgdropdown = document.getElementById("imgdropdown");
   let prevScrollpos = window.pageYOffset;
 
+  //Main navbar will be hidden when code run for first time
   navbar.style.top = "-150px";
 
   window.onscroll = function () {
     const currentScrollpos = window.pageYOffset;
 
     if (currentScrollpos <= 25) {
+      //hidden Main Navbar out from layer -150px
       navbar.style.top = "-150px";
+      //Navbar dropdown will show
       imgdropdown.style.display = "block";
     } else if (prevScrollpos > currentScrollpos) {
+      //When scrolling to down, will show Main Navbar
       navbar.style.top = "0";
+      //Navbar dropdown hidden
       imgdropdown.style.display = "none";
     } else {
+      //showing back Main Navbar
       navbar.style.top = "0";
+      //Hidden navbar dropdown
       imgdropdown.style.display = "none";
     }
 
+    //saving posisition to current possition
     prevScrollpos = currentScrollpos;
   };
 
@@ -78,16 +88,18 @@ document.addEventListener("DOMContentLoaded", function () {
       const containerEvent = this.parentElement;
       const eventText = containerEvent.querySelector(".event-text");
 
-      const hiddenContent = document.getElementById("hiddenContent1"); // Ganti dengan ID yang sesuai
+      const hiddenContent = document.getElementById("hiddenContent1");
 
       eventText.classList.toggle("hiddenContent1");
 
       if (eventText.classList.contains("hiddenContent1")) {
         this.textContent = "Read Less -";
-        hiddenContent.style.display = "block"; // Tampilkan konten tersembunyi
+        // Showing hidden content event, and the button will change to read less-
+        hiddenContent.style.display = "block";
       } else {
+        // Hidden again content event hidden, and the button will change to read less+
         this.textContent = "Read More +";
-        hiddenContent.style.display = "none"; // Sembunyikan konten tersembunyi
+        hiddenContent.style.display = "none";
       }
     });
   });
@@ -96,19 +108,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const closeIcon = document.querySelector(".close-icon");
   const searchInput = document.getElementById("search-input");
 
+  //First posisition
   inputBox.style.display = "none";
   closeIcon.style.display = "none";
 
+  //when click icon searchIcon
   searchIcon.addEventListener("click", () => {
     inputBox.classList.add("open");
+    //searchIcon will hidden
     searchIcon.style.display = "none";
+    //Showing input box and closeIcon
     inputBox.style.display = "block";
     closeIcon.style.display = "block";
   });
-
+  //When click icon searchIcon
   closeIcon.addEventListener("click", () => {
     inputBox.classList.remove("open");
+    //inputBox hidden
     inputBox.style.display = "none";
+    //searchIcon will show again
     searchIcon.style.display = "block";
   });
 
